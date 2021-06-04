936ab9b65802b8954b9d172cc801ab3097dff715
Sagedurk <65038650+Sagedurk@users.noreply.github.com>
2021-06-04 18:13:19 +0200

    Update InteractableEditor.cs

--- Assets/Scripts/Systems/Interaction System/Editor/InteractableEditor.cs
+++ Assets/Scripts/Systems/Interaction System/Editor/InteractableEditor.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 [CustomEditor(typeof(Interactable))]
 public class InteractableEditor : Editor 
 {
+
+    List<bool> listFoldouts = new List<bool>();
+
     public override void OnInspectorGUI()
     {
         //base.OnInspectorGUI();
@@ -33,34 +36,67 @@ public class InteractableEditor : Editor
 
                     for (int i = 0; i < interactable.listOfDialogueBoxes.Count; i++)
                     {
+                        while(listFoldouts.Count < i + 1)
+                        {
+                            listFoldouts.Add(false);
+                        }
+
                         SerializedProperty dialogueInstance = dialogueList.GetArrayElementAtIndex(i);
-                        
-                        //Create Property Drawer and add dialogueInstance variables inside
-                        
-                        EditorGUILayout.PropertyField(dialogueInstance);
+
+                        SerializedProperty dialogueBox = dialogueInstance.FindPropertyRelative("dialogueBox");    
+                        SerializedProperty portrait = dialogueInstance.FindPropertyRelative("portrait");
+                        SerializedProperty dialogueChoice = dialogueInstance.FindPropertyRelative("dialogueChoice");
+
+                    
+                    listFoldouts[i] = EditorGUILayout.Foldout(listFoldouts[i], "Element" + i.ToString());
+                       
+                        if(listFoldouts[i])
+                        {
+                            EditorGUI.indentLevel = 1;
+                            EditorGUILayout.PropertyField(dialogueBox);
+                            EditorGUILayout.PropertyField(portrait);
+                            EditorGUILayout.PropertyField(dialogueChoice);
+
+                            if (dialogueChoice.enumValueIndex == (int)DialogueMaster.dialogueChoices.ACTIVE)
+                            {
+                                //add code here for showing dialogue option list
+
+                                
+                                Debug.Log("DIALOGUE CHOICE #" + i.ToString() + " IS ACTIVE!");
+                            }
+                            
+                            EditorGUI.indentLevel = 0;
+                        }
 
                     }
 
-                EditorGUILayout.BeginHorizontal();
-                if (GUILayout.Button("Add"))
-                {
-                    interactable.listOfDialogueBoxes.Add(new DialogueMaster.DialogueInstance());
-                }
-                if (GUILayout.Button("Remove"))
-                {
-                    //Remove Last Element
-                    interactable.listOfDialogueBoxes.RemoveAt(interactable.listOfDialogueBoxes.Count - 1);
-                }
-                EditorGUILayout.EndHorizontal();
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Label("");
+
+                    GUIStyle buttonStyle = new GUIStyle("button");
+                    buttonStyle.fontSize = 19;
+                    buttonStyle.fontStyle = FontStyle.Bold;
+                    buttonStyle.alignment = TextAnchor.MiddleCenter;
+
+                    if (GUILayout.Button("+", buttonStyle, GUILayout.Width(23), GUILayout.Height(23)))
+                    {
+                        interactable.listOfDialogueBoxes.Add(new DialogueMaster.DialogueInstance());
+                    }
+
+                    if (GUILayout.Button("-", buttonStyle, GUILayout.Width(23), GUILayout.Height(23)))
+                    {
+                        //Remove Last Element
+                        interactable.listOfDialogueBoxes.RemoveAt(interactable.listOfDialogueBoxes.Count - 1);
+                        listFoldouts.RemoveAt(listFoldouts.Count - 1);
+                    }
+                    EditorGUILayout.EndHorizontal();
 
 
-                EditorGUILayout.Space();
-                    EditorGUILayout.Space();
                     EditorGUILayout.PropertyField(serializedObject.FindProperty("eventDialogue"));
                     serializedObject.ApplyModifiedProperties();
 
 
-                    break;
+                break;
 
                 case Interactable.InteractionType.CHEST:
                     Debug.Log("Chest");
